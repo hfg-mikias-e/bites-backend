bdf190ec6c69a8b4b1a27f4b8f5d3228c14c77f3
hfg-mikias-e <72493307+hfg-mikias-e@users.noreply.github.com>
2023-12-26 18:16:52 +0100

    very cool

--- index.js
+++ index.js
@@ -1,54 +1,50 @@
-const express = require('express')
-const app = express()
-const port = 4000
+require("dotenv").config();
+const express = require("express");
+const { ObjectId } = require("mongodb");
+const app = express();
 
 app.use(express.json());
 app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header(
-        "Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content-Type, Accept"
-    );
-    next();
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
 });
 
 // MongoDB-Client
-const MongoClient = require('mongodb').MongoClient;
+const MongoClient = require("mongodb").MongoClient;
 const uri = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_ID}.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri);
+let database;
+
+// Verbindung herstellen
+connectDB();
 
 // Funktion stellt Verbindung zur Datenbank her
 async function connectDB() {
-    try {
-        await client.connect();
-        console.log("Erfolgreich mit Datenbank verbunden!")
+  try {
+    await client.connect();
+    console.log("Erfolgreich mit Datenbank verbunden!");
 
-        return collections = {
-            achievements: {
-                badges: client.db("achievements").collection("badges")
-            },
-            bites: {
-                area: client.db("bites").collection("area"),
-                categories: client.db("bites").collection("categories"),
-                levels: client.db("bites").collection("levels"),
-                library: client.db("bites").collection("library"),
-            },
-            users: client.db("users").collection("links")
-        }
-
-    } catch {
-        console.error("Konnte keine Verbindung zur Datenbank herstellen.")
-        await client.close();
-    }
+    database = {
+      badges: client.db("achievements").collection("badges"),
+      area: client.db("bites").collection("area"),
+      categories: client.db("bites").collection("categories"),
+      levels: client.db("bites").collection("levels"),
+      library: client.db("bites").collection("library"),
+      profile: client.db("users").collection("profile"),
+    };
+  } catch {
+    console.error("Konnte keine Verbindung zur Datenbank herstellen.");
+    await client.close();
+  }
 }
 
-// Verbindung herstellen
-const database = connectDB()
-let stat = 500
-
 /*
-    let eintrag = await database.findOne({
-        adminCode: short
+    const eintrag = await database.area.findOne({
+        _id: new ObjectId('65897af7e3d0861685e3a4da')
     });
 
     await database.insertOne({
@@ -70,29 +66,40 @@ let stat = 500
     );
 */
 
-// "edit" GET-Endpoint ermittelt zugehörigen Datenbank-Eintrag für den Admin-Code
-app.get('/edit/:inputcode', async (req, res) => {
-    //in req.params.inputcode steckt das was im Browser angehängt wurde!
+app.get("/getCategories", async (req, res) => {
+  try {
+    const areas = await database.area.find().toArray();
 
-    // suche und übergebe zugehörigen Eintrag für Admin-Code
-    let data = await database.findOne({
-        adminCode: req.params.inputcode
-    });
-
-    if (data) {
-        // übergebe Informationen
-        res.status(200).send(data)
-    } else {
-        // Eintrag / URL existiert nicht
-        res.status(stat).end()
+    let data = [];
+    for (area of areas) {
+        const categories = await database.categories.find({area: area._id,}).toArray();
+        data.push({ name: area.name, skills: categories });
     }
-})
 
-// "check" POST-Enpoint gibt Status zurück, falls Verbindung mit Backend besteht
-app.post('/check', async (req, res) => {
-    res.status(200).end()
-})
+    res.status(200).send(data);
+  } catch {
+    console.error("categories could not be fetched");
+    res.status(500).end();
+  }
+});
 
-app.listen(port, () => {
-    console.log('Example app listening!')
-})
+app.post("/getSkills", async (req, res) => {
+  try {
+    console.log(req.body.categoryId)
+    const skills = await database.library.find({category: new ObjectId(req.body.categoryId),}).toArray();
+    console.log(skills);
+    res.status(200).send(skills);
+  } catch {
+    console.error("categories could not be fetched");
+    res.status(500).end();
+  }
+});
+
+// "check" Endpoint gibt Status zurück, falls Verbindung besteht
+app.get("/check", async (req, res) => {
+  res.status(200).end();
+});
+
+app.listen(4000, () => {
+  console.log("Example app listening!");
+});
