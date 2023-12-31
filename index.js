4b6f3d798a7f3479890744bdc8cf6f0d9acf0ace
hfg-mikias-e <mikias.ehrmann@hfg.design>
2023-12-31 13:12:58 +0100

    Update index.js

--- index.js
+++ index.js
@@ -34,8 +34,10 @@ async function sendPushNotification(external_id) {
     include_aliases: {
       external_id: [external_id]
     },
+    include_external_user_ids: [external_id],
     //included_segments: ["Test Users"],
     target_channel: "push",
+    channel_for_external_user_ids: "push",
     external_id: external_id
     /*
     data: {
