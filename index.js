167891d5a775eed4257852f06bfb9119693d49a4
hfg-mikias-e <mikias.ehrmann@hfg.design>
2024-01-21 17:50:18 +0100

    Update index.js

--- index.js
+++ index.js
@@ -92,7 +92,7 @@ async function sendPushNotification(external_id, content, date) {
     },
     send_after: date, //"2023-12-31 16:05:00 GMT+0100"
     target_channel: "push",
-    app_url: "https://driven-by-future-skills.vercel.app/reminder/" + content.id
+    url: "https://driven-by-future-skills.vercel.app/reminder/" + content.id
   };
 
   await cancelPushNotification(external_id, content.id)
